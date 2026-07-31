6c5aedf9dbcd84038ee6dc7b9340350f1f57f86b
ksj-1nom1 <godnswl1@gmail.com>
2026-07-31 19:11:15 +0900

    1절 가사 추가 (동해물과백두산이)

--- src/main/java/com/ohgiraffers/aegukga/Sing.java
+++ src/main/java/com/ohgiraffers/aegukga/Sing.java
@@ -5,7 +5,7 @@ public class Sing {
     public static void main(String[] args) {
 
         // 1절 출력부
-
+        System.out.println("동해물과 백두산이");
         // 후렴구 출력부
         System.out.println("무궁화 삼천리 화려강산" +
                 "대한사람 대한으로 길이 보전하세");
