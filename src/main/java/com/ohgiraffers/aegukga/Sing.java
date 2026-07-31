bb62972e711e738ab629995d0dc3db8266b02dcc
seun6h0925-rgb <seun6h0925@gmail.com>
2026-07-31 18:55:42 +0900

    creat sing java class file

---
+++ src/main/java/com/ohgiraffers/aegukga/Sing.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.aegukga;
+
+public class Sing {
+
+    public static void main(String[] args) {
+
+        // 1절 출력부
+
+        // 후렴구 출력부
+        System.out.println("무궁화 삼천리 화려강산" +
+                "대한사람 대한으로 길이 보전하세");
+    }
+}
